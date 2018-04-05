3218900777077a702b6207959662b5f92e7a191b
Karen Flores <karen.ag.flores@gmail.com>
2018-04-05 12:25:57 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -64,10 +64,6 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-programmer_hash[:yukihiro_matsumoto => {
-  :known_for => "Ruby",
-  :languages => ["LISP", "C"]
-  }]
 
 	programmer_hash =
  		{
@@ -84,6 +80,10 @@ programmer_hash[:yukihiro_matsumoto => {
           :languages => ["C"]
         }
      }
+     programmer_hash[:yukihiro_matsumoto => {
+       :known_for => "Ruby",
+       :languages => ["LISP", "C"]
+       }]
 
 programmer_hash
 end
